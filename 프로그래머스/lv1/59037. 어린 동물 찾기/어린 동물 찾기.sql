24cb2ad723540f99fa4b627fc891833de3cb6c7c
Daniel <99172504+zeromok@users.noreply.github.com>
2022-11-02 10:31:52 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,5 +1,8 @@
 -- 코드를 입력하세요
+-- <> : !=, not like
+-- 예문에서 오름차순으로 출력되어 있어서 ORDER BY 컬럼(ASC, DESC) : Default Value ASC
 SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION <> 'Aged'
 ORDER BY ANIMAL_ID;
+
