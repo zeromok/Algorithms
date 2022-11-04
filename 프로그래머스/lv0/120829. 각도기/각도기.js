50ef998e474d5ca340bb0bdf2557f7d293b26731
Daniel <99172504+zeromok@users.noreply.github.com>
2022-11-04 15:05:18 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,9 @@
+// 프로그램?의 오류로 순서와 형식을 조금 바꾸어 제출
+function solution(num) {
+ 
+    if(num == 180){ return 4; }
+    if(num < 90){ return 1; }
+    if(num == 90){ return 2; }
+    if(90 < num < 180){ return 3; }
+    
+}
