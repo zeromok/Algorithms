8d4348e566e3ee8f0a33aaa1e980012bbbc4856a
unknown <byoung.mokk@gmail.com>
2022-11-08 08:57:50 +0900

    짝수의 합 Modified

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -1,9 +1,22 @@
+/*
+    내가 푼 코드이지만 뭔가 이상해 수정하려 한다.
+    이상한점 : 반복문 안에 선언한 i 가 필요 없어 보임
+               반복문을 조금 수정하면 될 것 같다.
+*/
 class Solution {
     public int solution(int n) {
         
         int sum = 0;
         
-        for(int i=0; 0<=n; i++){
+        /* for(int i=0; 0<=n; i++){
+            if(n%2 == 0){
+                sum += n;
+            }
+            n--;
+        } */
+
+        // 변수 i를 활용하는 쪽으로 수정
+        for(int i=n; 0<=i; i--){
             if(n%2 == 0){
                 sum += n;
             }
@@ -12,3 +25,4 @@ class Solution {
         return sum;
     }
 }
+
--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,8 +1,16 @@
 function solution(n) {
     let sum = 0;
     
-    for(let i=0; 0<=n; i++ ){
+    /* for(let i=0; 0<=n; i++ ){
         if(n%2 == 0){
+            sum += n;
+        }
+        n--;
+    } */
+
+    // 변수 i를 활용하는 쪽으로 수정
+    for(let i=n; i>=0; i--){
+        if(n%2 ==0){
             sum += n;
         }
         n--;
