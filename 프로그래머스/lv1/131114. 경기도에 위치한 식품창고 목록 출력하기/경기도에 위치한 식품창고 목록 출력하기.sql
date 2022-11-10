b7242f41d4414d7e85dcd1637a2a771d13a9b4fd
zeromok <byoung.mokk@gmail.com>
2022-11-10 10:50:30 +0900

    131114. Solution Update

--- 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/lv1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,4 +1,8 @@
 -- 코드를 입력하세요
+/*
+    NULL값을 치환해주는 NVL() 함수를 사용해 해결 ***오라클에서만 제공하는 함수!
+    NVL(조회할 컬럼, null일경우 치환할 값) 
+*/
 SELECT WAREHOUSE_ID, WAREHOUSE_NAME, ADDRESS, NVL(FREEZER_YN, 'N') FREEZER_YN
 FROM FOOD_WAREHOUSE
 WHERE ADDRESS LIKE '경기도%'
