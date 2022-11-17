fcc6fe16adf10cac3b4262d95bc0b6dd7608d1de
Daniel <byoung.mokk@gmail.com>
2022-11-17 09:17:19 +0900

    120814. Update

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,6 +1,11 @@
 class Solution {
-    public int solution(int n) {
+    public static int solution(int n) {
         int answer = (int)Math.floor( (n-1) / 7 ) + 1;
+        // int answer = (int)Math.ceil( n / 7.0 );
         return answer;
     }
 }
+/*
+    Math.ceil(); 를 사용하는 방법
+    - ceil함수의 매개변수로는 double 타입이 들어와야 한다.
+*/
