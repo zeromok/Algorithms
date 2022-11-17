0cc7268fb0794fa04a736e9aea8b0f6b61fb3501
Daniel <byoung.mokk@gmail.com>
2022-11-17 09:52:32 +0900

    120821. Update

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -3,5 +3,14 @@ function solution(num_list) {
     for(let i=0; i<num_list.length; i++) {
         answer.unshift(num_list[i]);
     }
+    // var answer = num_list.reverse();
     return answer;
 }
+/*
+    풀이
+    - 배열에 맨 앞에 요소를 추가해주는 unshift() 를 사용했다.
+    - 추가하면서 num_list 의 처음요소가 마지막에 위치한다.
+
+    다른 방법
+    - reverse() 사용 하기
+*/
