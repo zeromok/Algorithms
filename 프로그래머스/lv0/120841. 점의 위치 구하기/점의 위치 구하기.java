ece5b319e777f3298a57e637a899e2472e2d1aa7
zeromok <byoung.mokk@gmail.com>
2022-11-23 16:05:22 +0900

    120841. 점의 위치 구하기 Update

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -2,8 +2,8 @@ class Solution {
     public int solution(int[] dot) {
         int x = dot[0];
         int y = dot[1];
-    
         int result = 0;
+
         if(x > 0 && y > 0) {
             result = 1;
         }else if(x < 0 && y > 0) {
@@ -16,3 +16,22 @@ class Solution {
         return result;
     }
 }
+/*
+    자바스크립트와 어떠한 점이 다른가? -> return의 쓰임
+
+    JAVA의 return : 그 즉시, 해당 "메소드"를 중지하고, void인경우 중지, 반환타입이 있는 메소드라면? 반드시 반환타입을 리턴해줘야 한다.
+                  : 위 자바코드가 JS코드와 다른 이유는 "모든상황을 커버할수 있게" 만들어 줬기 때문이다. (마지막에 꼭 else로 끝내기) -> 조금은 불편한 코드
+
+    JS의 return : 그 즉시, 함수 실행을 중지하고, (값을 제공한 경우)주어진 값을 함수 호출 지점으로 반환
+
+    =========
+    다른 풀이 -> 삼항연산자 사용
+    =========
+    int x = dot[0];
+    int y = dot[1];
+
+    if(x>0){
+        return y>0 ? 1 : 4;
+    }
+    return y>0 ? 2 : 3;
+*/
--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -8,7 +8,19 @@ function solution(dot) {
         return 2;
     }else if(x < 0 && y < 0) {
         return 3;
-    }else if(x > 0 && y < 0) {
+    }else {
         return 4;
     }
 }
+/*
+    =========
+    다른 풀이 -> 삼항연산자 사용
+    =========
+    let x = dot[0];
+    let y = dot[1];
+
+    if(x>0){
+        return y>0 ? 1 : 4;
+    }
+    return y>0 ? 2 : 3;
+*/
