2fcbad3724e2e2fd71080e56df6ed42fe39951c7
Daniel <byoung.mokk@gmail.com>
2022-11-27 14:41:08 +0900

    59047.이름에 el이 들어가는 동물 찾기 Update

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -3,3 +3,8 @@ SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE ANIMAL_TYPE = 'Dog' AND UPPER(NAME) LIKE '%EL%'
 ORDER BY NAME;
+/*
+    AND : 앞, 뒤 두 가지 조건을 모두 만족해야만 검색가능
+    LIKE : ~와 같은 조건 Ex) %EL% - 컬럼 값에 위치 상관없이 EL이 들어가면 뽑아와줌
+    UPPER(컬럼) : 컬럼의 값을 모두 대문자로 변환
+*/
