21a09c00712e0f5e13ac445ff2cbae2bebb0af07
Daniel <byoung.mokk@gmail.com>
2022-11-27 12:55:50 +0900

    120825.문자 반복 출력하기 Update

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -8,3 +8,10 @@ function solution(my_string, n) {
     }
     return answer.join('');
 }
+/*
+    나의 풀이법 :
+    1. my_string(문자열)의 각 문자를 n번 반복한 문자열을 반환해줘야한다.
+    2. 빈 배열을 만든다
+        -> 문자열을 순회하며 각 문자를 n번 반복해 넣는다
+        -> 배열을 문자열로 반환
+*/
