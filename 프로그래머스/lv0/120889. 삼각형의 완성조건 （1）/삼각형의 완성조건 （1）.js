ce7fe2008d07acc4fd7573eec7492ebf4e25a36a
Daniel <byoung.mokk@gmail.com>
2022-11-27 10:49:41 +0900

    120889.삼각형의 완성조건(1) Update

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -2,3 +2,9 @@ function solution(sides) {
     newArr = sides.sort();
     return newArr[newArr.length - 1] >= newArr[0] + newArr[1] ? 2 : 1;
 }
+/*
+    나의 풀이 :
+    1. 들어온 배열을 .sort() 하면 끝 인덱스의 값은 가장 큰변이 된다.
+    2. 가장 큰 변을 앞 2개의 인덱스 값 합에 비교 후 1 또는 2 반환 하도록 삼항연산자 비교
+    3. 가장 큰 변 >= 나머지 변의 합 ? 2 : 1; -> 같을 경우 2(만들 수 없음) 반환
+*/
