04ac98f04bcd238aaf4ef0753fe88df54bb0b8f0
Daniel <byoung.mokk@gmail.com>
2022-11-27 15:55:27 +0900

    59410.NULL 처리하기.sql Update

--- 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/lv2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -2,3 +2,10 @@
 SELECT ANIMAL_TYPE, NVL(NAME,'No name') AS NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
+/*
+    접근 방법 :
+    1. NAME 의 NULL값을 No name 으로 나오게 만들어야 한다.
+    2. NULL 값을 치환해주는 함수 = NVL() 을 사용해보자
+    3. NVL(컬럼, NULL일 경우 치환 할 값)
+       Ex) NVL(NAME,'No name') : NAME컬럼 안 NULL값을 모두 'No name'로 치환
+*/
