9bfdc7102477df1fc1c60b2e9aa17db20cca2c12
zeromok <byoung.mokk@gmail.com>
2022-11-29 14:59:48 +0900

    120847.최댓값 만들기(1).js Update

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -3,3 +3,9 @@ function solution(numbers) {
     let result = numbers[0] * numbers[1];
     return result;
 }
+/*
+    접근법 : 
+    1. 매개변수로 들어온 배열의 원소의 곱 중 최대값 = 정렬 후 1,2번 째 원소
+    2. arr = 매개변수로 들어온 배열의 내림차순
+    3. 즉, arr의 0, 1번째 인덱스가 최대값 = 두 값을 곱하면 최대곱
+*/
