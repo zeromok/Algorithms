c2dfb6915d3ebed00447552d2632a116426f66d3
zeromok <byoung.mokk@gmail.com>
2022-11-30 09:11:10 +0900

    120826.특정 문자 제거하기.java Update

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -4,3 +4,9 @@ class Solution {
         return result;
     }
 }
+/*
+    접근법 :
+    JAVA의 replaceAll(정규식, 치환문자); 을 사용하였다.
+    매개값으로 정규식을 받기 때문에 사용에 주의가 필요해 보여 
+    my_string.replace(기존문자, 치환문자); 으로 사용하는게 더 좋을것 같다.
+*/
