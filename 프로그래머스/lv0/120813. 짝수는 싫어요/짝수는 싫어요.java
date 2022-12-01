7aef2e38729185d585df165604175372a7c071b1
Daniel <99172504+zeromok@users.noreply.github.com>
2022-12-01 09:18:53 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> list=new ArrayList<Integer>(); 
+        
+        for(int i=1; i<=n; i+=2) {                        
+            list.add(i);                                
+        }
+        
+        return list;
+    }
+}
