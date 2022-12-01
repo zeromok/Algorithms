b6f7e070710b8f42f2ace28720c0a2a9d9f9a80e
zeromok <byoung.mokk@gmail.com>
2022-12-01 09:30:42 +0900

    짝수는 싫어요.js Update

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -7,3 +7,13 @@ function solution(n) {
     }
     return result;
 }
+/*
+    접근법 :
+    1. 0~n 개까지 순회 하면서 조건문을 달아 홀수만 배열에 저장
+    2. 홀수만 들어갈 배열 선언
+    3. n개까지 순회하는 for문 선언
+    4. for문 안 홀수만 판별해내는 if문 선언후 통과되면 배열에 추가
+
+    다른 풀이 :
+    filter( (value) => { result value%2 === 1; } )
+*/
