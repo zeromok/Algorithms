129acc4893436bbc45783d8afb1a96f8f0482d21
zeromok <byoung.mokk@gmail.com>
2022-12-02 08:37:33 +0900

    120836.순서쌍의 개수.js Update

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -7,3 +7,21 @@ function solution(n) {
     }
     return arr.length;
 }
+/*
+    접근법 :
+    1. 순서쌍 = 약수들의 곱 = 약수의 개수
+    2. 1~n 개 까지 반복하며 나머지가 0 인 숫자(약수)들을 모아 배열에 넣자
+    3. 약수들이 모인 배열의 길이를 반환하면 정답
+
+    다른 풀이 :
+    1. 배열에 넣지 않고 count변수(result)를 선언해 해결할 수도 있다.
+        function solution(n) {
+            let result = 0;
+            for(let i=1; i<=n; i++) {
+                if(n%i === 0) {
+                    result++;
+                }
+            }
+            return result;
+        }
+*/
