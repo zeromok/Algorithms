72db2a9edcb7d77d576991802995ac4ba05625ca
zeromok <byoung.mokk@gmail.com>
2022-12-02 08:46:12 +0900

    120836.순서쌍의 개수.java Update

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -1,12 +1,13 @@
- class Solution {
+import java.util.ArrayList;
+class Solution {
     public int solution(int n) {
-        int result = 0;
-        for(int i=1; i<=n; i++) {
-            if(n%i == 0) {
-                result++;
-            }
+        ArrayList<Integer> list=new ArrayList<Integer>(); 
+        
+        for(int i=1; i<=n; i++) {                        
+            list.add(i);                                
         }
-        return result;
+        
+        return list.size();
     }
 }
 /*
