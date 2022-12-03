a7aee4b71e57cb3c9ab80164bbe40e8de7527dc4
Daniel <byoung.mokk@gmail.com>
2022-12-03 23:21:09 +0900

    120811.중앙값 구하기.java Update

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -10,7 +10,7 @@ class Solution {
     접근법 :
     JS 와는 다르게 자바는 역시 타입 때문에 생각이 좀 길어진다.
     내장함수가 없으므로, Arrays 를 임포트해 .sort() 메소드 사용
-    Math.floor() 를 이용해 중앙의 인덱스 찾아내기 double 타입으로 반환되므로, int로 형변환
+    Math.floor() 를 이용해 중앙의 인덱스 찾아내기 double 타입을 받고/반환되므로, int로 형변환
 
     **주의 :
     Math 클래스의 모든 메소드는 클래스 메소드(static method)이므로,
