486f843583d7f524df505f47f3ad235f7a5ec757
Daniel <99172504+zeromok@users.noreply.github.com>
2022-12-04 19:43:15 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+class Solution {
+    public String solution(String my_string) {
+        String result = my_string.replaceAll("[a,e,i,o,u]", "");
+        return result;
+    }
+}
