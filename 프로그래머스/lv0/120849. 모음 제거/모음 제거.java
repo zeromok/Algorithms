d3edb2a6c9d450f2ff0d673301e9ee080c40a4e1
Daniel <byoung.mokk@gmail.com>
2022-12-04 21:02:10 +0900

    120849.모음제거.java Update

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -4,3 +4,22 @@ class Solution {
         return result;
     }
 }
+/*
+    접근법 :
+    위에서는 JS에서 얻은 힌트를 사용해 쉽게 풀었다.
+
+    다른 풀이 :
+    class Solution {
+        public String solution(String my_string) {
+
+            String str = "aeiou";
+            for(char c : str.toCharArray()) { -> str 을 문자하나하나 char타입의 배열로 변환
+                my_string = my_string.replace(String.valueOf(c), "");
+                -> String.valueOf(c) (char타입의 원소(c)를 String으로 변환)
+                -> replace() 를 사용해 치환
+            }
+            return my_string;
+
+        }
+    }
+*/
