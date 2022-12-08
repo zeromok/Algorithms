466e9dfa5efc403bdfe9888205d62bec466f4249
zeromok <byoung.mokk@gmail.com>
2022-12-09 08:59:52 +0900

    120910.세균 증식.js Update

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -2,3 +2,11 @@ function solution(n, t) {
     let result = n * Math.pow(2, t);
     return result;
 }
+/*
+    접근법 :
+    시간(t) - 처음 세균 수 2(n)
+    1시간 후 = 2(n) * 2의1(t)승
+    2시간 후 = 2(n) * 2의2(t)승 ...
+
+    제곱을 구해주는 함수 Math.pow() 를 사용해 쉽게 접근
+*/
