8f6f028d8fe100bc4308d523a085f11c51832b9c
zeromok <byoung.mokk@gmail.com>
2022-12-09 09:01:32 +0900

    120910.세균 증식.java Update

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -3,3 +3,12 @@ class Solution {
         return n * (int)Math.pow(2, t);
     }
 }
+/*
+    접근법 :
+    시간(t) - 처음 세균 수 2(n)
+    1시간 후 = 2(n) * 2의1(t)승
+    2시간 후 = 2(n) * 2의2(t)승 ...
+
+    JAVA 에서는 Math.pow() 의 리턴, 매개변수타입 -> bouble타입 !
+    나온 값을 현변환해줘서 반환한다.
+*/
