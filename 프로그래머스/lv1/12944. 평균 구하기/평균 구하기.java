945615deeb24fd1a5470d87c78942eba31d815bd
Daniel <byoung.mokk@gmail.com>
2022-12-11 08:00:08 +0900

    12944.평균 구하기.java Update

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -7,3 +7,19 @@ class Solution {
         return (double)sum/arr.length;
     }
 }
+/*
+    접근법 :
+    평균 = 요소들의 합 / 배열의 길이
+    정석적인? 풀이 방법으로 풀어 보았다.
+
+    다른 접근법 : 향상된 포문 사용
+    class Solution {
+        public double solution(int[] arr) {
+            int sum = 0;
+            for(int element : arr) {
+                sum += element;
+            }
+            return (double)sum/arr.length;
+        }
+    }
+*/
