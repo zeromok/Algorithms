2b3e9362020f554954c6603a7b58744d43cf9d1c
zeromok <byoung.mokk@gmail.com>
2022-12-12 13:00:39 +0900

    120905.n의 배수 고르기.js Update

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -2,3 +2,9 @@ function solution(n, numlist) {
     let result = numlist.filter( (element) => { return element%n === 0 });
     return result;
 }
+/*
+    접근법 :
+    .filter() : 콜백 함수를 통과해 true인 요소만 모아 새로운 배열 반환
+
+    element(요소)%n === 0 즉, n으로 나누었을 때, 0인 요소 = n 의 배수이다.
+*/
