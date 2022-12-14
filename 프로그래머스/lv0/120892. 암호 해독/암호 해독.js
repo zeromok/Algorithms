f6a8e490a4e0967b0a2cb99327da348d2a4ec73f
zeromok <byoung.mokk@gmail.com>
2022-12-14 14:16:48 +0900

    120892.암호 해독.js Update

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -6,3 +6,32 @@ function solution(cipher, code) {
     }
     return result;
 }
+/*
+    접근법 :
+    들어온 문자열을 배열로 만들어 -> .split();
+    code의 배수들만 뽑아 문자열을 만들어 반환 -> for문
+
+
+    다른 접근법 :
+    처음에는 filter()를 사용해보려다 인덱스에서 막혀 헤매다 해결...!
+    
+    1. .filter(); 사용
+        배열의 각 요소에 대해 콜백함수를 실행 후 true인 값만 반환
+
+    function solution(cipher, code) {
+        let result = cipher.split('').filter( (data, index, arr) => {
+            return (index+1)%code === 0;
+        });
+        return result.join('');
+    }
+
+    2. .reduce(); 사용
+        배열의 각 요소에 대해 콜백함수를 실행 후 마지막 누적값(acc)를 반환
+        
+    function solution(cipher, code) {
+        let result = cipher.split('').reduce( (acc, cur, index) => {
+            return (index+1)%code === 0 ? acc + cur : acc;
+        }, '');
+        return result;
+    }
+*/
