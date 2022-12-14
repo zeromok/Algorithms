1a9cd6b9d54f9b3550d90de5c7d051f5f719ba7c
zeromok <byoung.mokk@gmail.com>
2022-12-14 09:28:05 +0900

    120845.주사위의 개수.java Update

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -7,3 +7,21 @@ class Solution {
         return result;
     }
 }
+/*
+    접근법 :
+    for문을 돌며 원소들을 n으로 나누어 누적 곱으로 반환했다.
+
+
+    다른 접근 : 향상된 포문을 이용할 수 있을까?
+
+    1. 이용할 수 있다.
+    Class Solution {
+        public int solution(int[] box, int n) {
+            int result = 1;
+            for(int e : box) {
+                result = result * (int)Math.floor(e/n);
+            }
+            return result;
+        }
+    }
+*/
