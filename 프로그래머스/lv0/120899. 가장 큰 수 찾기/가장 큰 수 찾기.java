4c77223f58ae70e9665c9f3959001e16d68ed43b
zeromok <byoung.mokk@gmail.com>
2022-12-16 16:04:00 +0900

    120899.가장 큰 수 찾기.java Update

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -2,11 +2,30 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array) {
         int[] result = new int[2];
+
         int[] copy = array.clone();
         Arrays.sort(copy);
+
         int num = copy[copy.length -1];
+
         result[0] = num;
         result[1] = Arrays.binarySearch(array, num);
+
         return result;
     }
 }
+/*
+    접근법 :
+    결과 값이 들어갈 배열을 선언해준다. 크기 : 2
+    .clone()
+        매개변수로 들어온 배열[일반자료형] 을 깊은 복사
+    Arrays.sort()
+        복사된 배열의 정렬
+    Arrays.binarySearch();
+        자바에서의 배열은 indexOf()를 지원하지 않는다. -> 그래서 이 메소드를 쓴다.
+        이진 탐색 알고리즘을 이용한 메소드 int[정렬된 배열]의 값, int형인자를 받아 위치를 찾아준다. 
+        
+    
+    다른 접근법 :
+    
+*/
