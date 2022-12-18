5eec3b0bf4e4b3bc9183a52100473fc2f7423e5c
Daniel <byoung.mokk@gmail.com>
2022-12-18 12:45:35 +0900

    12937.짝수와 홀수.java Update

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -9,3 +9,18 @@ class Solution {
        }
     }
 }
+/*
+    접근법 :
+    타입을 생각할 부분이 없어서 JS랑 같은 방식으로 접근했다.
+
+
+    다른 접근법 :
+    삼항 연산자를 이용한 접근
+    
+    class Solution {
+        public String solution(int num) {
+            String result = (num%2 == 0) ? "Even" : "Odd";
+            return result;
+        }
+    }
+*/
