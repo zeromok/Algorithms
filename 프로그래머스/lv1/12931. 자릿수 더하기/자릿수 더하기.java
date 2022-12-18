5471ad76139bdb52cfe3c7086c096d243862a37a
Daniel <byoung.mokk@gmail.com>
2022-12-18 14:06:31 +0900

    12931.자릿수 더하기.java Update

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -14,3 +14,33 @@ public class Solution {
         return sum;
     }
 }
+/*
+    접근법 :
+    들어온 자연수(n)를 String 으로 변환 -> Integer.toString(n);
+    for문을 통해 각 자리 문자에 접근 해 int 로 바꾼후 누적 -> .charAt(i) - '0';
+
+
+    다른 접근법 :
+        (n + "")   : 빈 문자열을 더해 자연수 -> 문자열
+    .toCharArray() : 문자열 -> 배열
+    향상된 for문을 사용해 원소들을 뽑아 누적
+    
+    import java.util.*;
+    public class Solution {
+        public int solution(int n) {
+
+            String result = n + "";
+            char[] str = result.toCharArray();
+            int sum = 0;
+
+            for(char e : str) {
+                sum += e - '0';
+            }
+
+            // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+            System.out.println("Hello Java");
+
+            return sum;
+        }
+    }
+*/
