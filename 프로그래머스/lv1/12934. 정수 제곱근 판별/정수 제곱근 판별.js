58331744780fc20027d973312d847f340621ac13
Daniel <99172504+zeromok@users.noreply.github.com>
2022-12-21 08:36:11 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let sqrt = Math.sqrt(n);
+    let result = Math.pow( (sqrt + 1) , 2);
+    if( !Number.isInteger(sqrt) ) {
+        return -1;
+    }
+    return result;
+}
