b3fc2c7e43ef420fd0b5a5e4cb14dd8dfc5b3312
zeromok <byoung.mokk@gmail.com>
2022-12-21 08:47:41 +0900

    Code Sort

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,8 +1,12 @@
 function solution(n) {
+
     let sqrt = Math.sqrt(n);
     let result = Math.pow( (sqrt + 1) , 2);
+
     if( !Number.isInteger(sqrt) ) {
         return -1;
     }
+
     return result;
+    
 }
