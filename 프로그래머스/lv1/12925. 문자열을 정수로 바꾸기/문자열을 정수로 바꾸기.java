b9d30cd9bfc81cda2f3715063128e8a292f010dc
Daniel <byoung.mokk@gmail.com>
2022-12-28 22:02:45 +0900

    Refactor: Add Comments

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -4,3 +4,9 @@ class Solution {
         return result;
     }
 }
+/*
+    접근법 :
+
+    1. Integer.parseInt() : 문자열을 기본형 정수 (primitive type int)로 리턴
+    2. Integer.valueOf() : 문자열을 정수 객체(integer object)로 리턴
+*/
