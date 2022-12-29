6527d7ee08d44f8c41fcef0959947ef1581c72c8
Daniel <99172504+zeromok@users.noreply.github.com>
2022-12-29 21:08:37 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,12 @@
+function solution(x) {
+    // 들어온 정수의 자릿수를 구함
+    // 각 자릿수를 더해 x에 나누어 0이면 true 아니면 false
+    
+    let str = String(x);
+    let sum = 0
+    let result = str.split("").forEach( (ele) => {
+        sum += Number(ele);
+    })
+    return x%sum === 0;
+    
+}
