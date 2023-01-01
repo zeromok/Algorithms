0c4042b3d4570ce393029515dcbf315948a5b431
Daniel <byoung.mokk@gmail.com>
2023-01-01 10:26:51 +0900

    Refactor: Add comments

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -11,8 +11,14 @@ class Solution {
 }
 /*
     접근법 :
-
+    JS 와 비슷한 방법으로 접근했다.
+    타입과 형변환 주의하기 !
 
     다른 접근법 :
+    자바에서 정수 -> 문자열 변환하는 방법
+        1. String.valueOf()
+        2. Integer.toString()
+        3. String.format("정규식", 정수)
+        4. 빈 문자열을 앞에 더해주면 변환 "" + 정수
     
 */
