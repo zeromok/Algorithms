7d8f6f4af986ffb95c25a2ac546cb66f230ed5d0
zeromok <byoung.mokk@gmail.com>
2023-01-07 17:05:31 +0900

    Add comments

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -9,5 +9,16 @@ function solution(n) {
 }
 /*
     접근법 :
+    n%0, 1, 2, 3, ... 나누는 값을 작은 수 부터 올라가며 대입해주면서
+    === 1일 때 바로 리턴이 되게끔 만들어 주었다.
+
     
-*/
+    다른 접근법 :
+    functrion solution(n) {
+
+        for(int i=1; i<n; i++) {
+            if(n%i === 1) { return i; }
+        }
+        
+    }
+*/                
