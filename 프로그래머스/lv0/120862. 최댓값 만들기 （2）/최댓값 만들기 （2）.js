f9c67faba7ba7a9a76a56db8dfbe6462fe822fde
Daniel <99172504+zeromok@users.noreply.github.com>
2023-01-08 19:46:22 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+    // 들어온 배열의 원소들의 곱 중 최댓값을 만들 수 있는 원소
+    // 다른말로 곱했을 때 가장 큰 정수 2개 뽑아라?
+    // 각각의 원소들을 곱해보면서 접근?
+    
+    let max = numbers[0] * numbers[1];
+    for(let i=0; i<numbers.length - 1; i++) {
+        for(let j=i+1; j<numbers.length; j++) {
+            if(max < numbers[i] * numbers[j]) {
+                max = numbers[i] * numbers[j];
+            }
+        }
+    }
+    
+    return max;
+     
+}
