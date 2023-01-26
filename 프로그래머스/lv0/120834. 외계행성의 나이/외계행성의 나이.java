b4b338f6d10498ed9e23e0ddf88b804aaf80cb1f
Daniel <byoung.mokk@gmail.com>
2023-01-26 21:39:52 +0900

    Study: Problem Solving

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -15,3 +15,13 @@ class Solution {
         return answer;
     }
 }
+/*
+    접근법 :
+    JS와 마찬가지로 아스키 코드로 접근해 풀어보았다.
+
+    String.valueOf(a) : a -> 문자열
+    (int)String.charAt(i) : i번째 문자 -> 아스키코드
+    (char)Number : Number -> 문자 = 아스키코드 -> 문자
+
+    자바는 형변환으로 되는구나... 뭔가 좀 더 괜찮은? 방법이 있을것 같고...
+*/
