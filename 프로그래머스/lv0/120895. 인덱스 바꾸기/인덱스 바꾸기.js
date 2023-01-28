4b43f0dc35c16b9a4211c2263fde2d1a356283e4
Daniel <byoung.mokk@gmail.com>
2023-01-28 10:33:28 +0900

    Study: Problem Solving

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,5 +1,5 @@
 function solution(my_string, num1, num2) {
-    let arr = my_string.split("");
+    const arr = my_string.split("");
     
     let str1 = arr[num1];
     let str2 = arr[num2];
@@ -10,6 +10,25 @@ function solution(my_string, num1, num2) {
     return arr.join("");
 }
 /*
-    ���ٹ� : 
+    접근법 : 
+    1. 문자열의 정해진 자리에 있는 문자들을 서로 바꾼다.
+    2. 문자열 -> 배열 === .split("");
+    3. 정해진 자리에 있는 문자들 변수에 담아놓기 === str1, str2
+    4. 기존에 있던 문자 제거하고 바뀔 문자로 체인지 === .splice(기존문자index, 1, 바뀔 문자);
 
+    알고보면 다 알고있고 쉽게 접근해 풀 수있는 문제이다.
+    적용이 어려워 그렇지...
+
+    arr.splice( start [, deleteCount [, item1 [, item2 [, ... ]]]] )
+    - start
+        배열의 변경을 시작할 인덱스(음수인 경우 끝에서 부터)
+    - delete
+        (From start) 제거할 요소의 수, 0 = 제거x
+    - item
+        배열의 추가할 요소
+
+
+    다른 접근법 :
+    replace(), substring() 으로도 접근을 못하고,, 다른 접근법이 보이지 않는다 ㅠㅠ
+    좀 더 생각보자
 */
