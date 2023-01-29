fd1e19e551c8f5c7879e9e96b8c024ec127fbfd2
Daniel <99172504+zeromok@users.noreply.github.com>
2023-01-28 18:00:46 -0800

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    // 박수횟수 = return
+    // 3, 6, 9 의 배수 카운트 즉, 3의 배수 카운트?
+    
+    let result = 0;
+    
+    let str = order.toString();
+    
+    [...str].forEach( item => {if(Number(item)%3 === 0 && Number(item)>=3 ) result++} )
+    
+    return result;
+}
