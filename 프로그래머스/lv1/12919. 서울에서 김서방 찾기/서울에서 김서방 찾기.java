150c6ed2f95681a45d90459e082e37a095791266
Daniel <byoung.mokk@gmail.com>
2023-03-12 12:28:44 +0900

    Study: 서울에서 김서방 찾기.java

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -9,3 +9,11 @@ class Solution {
         return "김서방은 " + result + "에 있다";
     }
 }
+/*
+* 접근법
+* 위와 같이 배열로 접근하는 방법 말고도
+* ArrayList 로 접근하는 방법이 있다.
+* Ex)
+* import java.util.Arrays;
+* int result = Arrays.asList(str).indexOf("Kim");
+* */
