388139cb433afe47753fcfbde6c84ea47d471c28
Daniel <byoung.mokk@gmail.com>
2023-03-14 15:48:57 +0900

    Study: 나누어 떨어지는 숫자 배열.java

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] arr, int divisor) {
-        int[] result = Arrays.stream(arr).filter(item -> item % divisor == 0).sorted().toArray();
+        int[] result = Arrays.stream(arr)
+                .filter(item -> item % divisor == 0) // 참인 요소들로 이루어진 배열 반환
+                .sorted()   // 정렬 default: 이름사전순
+                .toArray(); // 위에서 반환된 Stream객체 -> 배열
         if(result.length == 0) return new int[]{-1};
         return result;
     }
