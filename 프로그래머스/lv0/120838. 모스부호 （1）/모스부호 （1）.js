4e8470d1f08bd3cc9941f8761d7e4de609280cd4
Daniel <99172504+zeromok@users.noreply.github.com>
2023-03-16 12:56:25 +0900

    [level 0] Title: 모스부호 (1), Time: 0.62 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,16 @@
+function solution(letter) {
+    let morse = { '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+                '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+                '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+                '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+                '-.--':'y','--..':'z'};
+    let result = "";
+        for(let i = 0; i<letter.length; i++) {
+            for (let str in morse) {
+                if (letter.split(" ")[i] === str) {
+                    result += morse[str];
+                }
+            }
+        }
+        return result;
+}
