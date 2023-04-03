704e0321a4d8fc1e356ae9a6e612e2e132898590
Daniel <byoung.mokk@gmail.com>
2023-04-03 12:12:40 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(int n) {
-       int result = 1;
+
+       int result = 1; // 반환될 팩토리얼
+
         for (int i = 1; i <= 10; i++) {
             result *= i;
             if (result == n) return i;
             if (result > n) return i-1;
         }
+
         return result;
+
     }
 }
