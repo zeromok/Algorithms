12c67ec4f15c2270e61e64c5756c4e1b41bd94dc
Daniel <byoung.mokk@gmail.com>
2023-06-29 13:00:40 +0900

    Refactor: JS 리펙토링

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -2,14 +2,14 @@ function solution(a, d, included) {
     let comm = [];
     let trueIdx = [];
     let result = 0;
-    
+
     for (let i = 0; i < included.length; i++) {
         comm.push(a + (i * d));
         if (included[i]) trueIdx.push(i);
     }
 
-    for (let i = 0; i < trueIdx.length; i++) {
-        result += comm[trueIdx[i]];
+    for (const element of trueIdx) {
+        result += comm[element];
     }
     return result;
 }
