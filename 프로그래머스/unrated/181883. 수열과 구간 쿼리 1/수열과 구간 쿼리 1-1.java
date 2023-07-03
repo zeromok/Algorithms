5772018e04761925494f61b8b16558ab012fd798
Daniel <byoung.mokk@gmail.com>
2023-07-03 16:00:48 +0900

    Fix: .java 내용 수정

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1-1.java
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1-1.java
@@ -1,7 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
 	public int[] solution( int[] arr, int[][] queries ) {
 		Arrays.stream( queries )
-			  .flatMapToInt( query -> IntStream.rangeClosed( query[ 0 ], query[ 1 ] ) )
+			  .flatMapToInt(
+				  query -> IntStream.rangeClosed( query[ 0 ], query[ 1 ] ) ) // query[0] <= i <=query[1] 에 부합하는 Int 반환
 			  .forEach( i -> arr[ i ] += 1 );
 		return arr;
 	}
--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,12 +1,10 @@
 class Solution {
-    public int[] solution(int[] arr, int[][] queries) {
-        for ( int[] query : queries ) {
-			int s = query[ 0 ];
-			int e = query[ 1 ];
-			for ( int i = s; i <= e; i++ ) {
+	public int[] solution( int[] arr, int[][] queries ) {
+		for ( int[] query : queries ) {
+			for ( int i = query[ 0 ]; i <= query[ 1 ]; i++ ) {
 				arr[ i ] += 1;
 			}
 		}
 		return arr;
-    }
+	}
 }
