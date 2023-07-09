5348169429853e05b8bcf084649ba39060f3a6d1
Daniel <99172504+zeromok@users.noreply.github.com>
2023-07-09 18:03:16 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 10.27 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051#) 
 
 ### 성능 요약
 
-메모리: 72.5 MB, 시간: 3.23 ms
+메모리: 83.7 MB, 시간: 10.27 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,7 +3,16 @@ import java.util.stream.IntStream;
 class Solution {
     public int solution(int[] numbers) {
         return IntStream.rangeClosed(0, 9)
-            .filter(idx -> Arrays.stream(numbers).noneMatch(number -> number == idx))
-            .sum();
+						.filter(idx -> {
+							int[] sortedNumbers = Arrays.stream(numbers)
+														.sorted()
+														.toArray();
+							System.out.println("idx: " + idx);
+							System.out.println("sortedNumbers: " + Arrays.toString(sortedNumbers));
+							System.out.println(Arrays.binarySearch(sortedNumbers, idx));
+							System.out.println("==================");
+							return Arrays.binarySearch(sortedNumbers, idx) < 0;
+						})
+						.sum();
     }
 }
