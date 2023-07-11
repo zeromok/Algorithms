1b6dc4e56aaa5b3694e17b0b748fc0fcf3dc743e
Daniel <byoung.mokk@gmail.com>
2023-07-11 10:45:00 +0900

    [unrated] Title: 세로 읽기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -1,9 +1,7 @@
 function solution(my_string, m, c) {
-    let result = "";
-    for (let i = 0; i < my_string.length; i++) {
-        if (i * m + m <= my_string.length) 
-            result += my_string.substring(i * m, i * m + m).charAt(c - 1);
-           
-    }
-    return result;
+  let result = "";
+  for (let i = 0; i < Math.ceil(my_string.length) / m; i++) {
+    result += my_string.substring(i * m, i * m + m).charAt(c - 1);
+  }
+  return result;
 }
