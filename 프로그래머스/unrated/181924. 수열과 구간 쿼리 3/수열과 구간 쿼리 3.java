2af061d7032f56526ea9b708c9d0bd3768d9238d
Daniel <99172504+zeromok@users.noreply.github.com>
2023-07-20 22:14:15 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 5.31 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -6,9 +6,7 @@ class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
     for (int[] query : queries) {
-        int temp = list.get(query[0]);
-        list.set(query[0], list.get(query[1]));
-        list.set(query[1], temp);
+        Collections.swap(list, query[0], query[1]);
     }
     return list.stream().mapToInt(Integer::intValue).toArray();
     }
