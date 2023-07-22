b666678a45c412cc24c65560998a831b96b82069
Daniel <byoung.mokk@gmail.com>
2023-07-22 12:49:49 +0900

    Refactor:

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3-1.js
@@ -0,0 +1,8 @@
+function solution(arr, queries) {
+    queries.forEach((queryp) => {
+        let temp = arr[query[0]];
+        arr[query[0]] = arr[query[1]];
+        arr[query[1]] = temp;
+    });
+    return arr;
+}
