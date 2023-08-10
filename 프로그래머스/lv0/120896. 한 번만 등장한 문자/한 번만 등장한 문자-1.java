d13f403e4da422afd6a433c4cfbd678650b96c3e
Daniel <byoung.mokk@gmail.com>
2023-08-10 21:42:44 +0900

    Refactor: 다른 접근법

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자-1.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자-1.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 class Solution {
     public String solution(String s) {
-		StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 		for ( String ch : s.split( "" ) ) {
 			if ( s.indexOf( ch ) == s.lastIndexOf( ch ) )
 				sb.append( ch );
