6e56bbde397e17ed723b6b85c167cf5703744b52
Daniel <byoung.mokk@gmail.com>
2023-08-16 20:48:58 +0900

    Refactor: 슈도 코드 작성

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main( String[] args ) {
+		/*
+		* N의 값 입력 받기
+		* 숫자 N개 입력 받아 String 으로 형변환
+		* N개의 크기를 가진 char형 배열 선언
+		* char형 배열에 String인 숫자 N개 형변환 해서 집어 넣기
+		* char형 순회하며 int형변환 해 누적값 계산 후 출력
+		* */
 		Scanner sc = new Scanner( System.in );
 		int num = sc.nextInt();
 		String strNum = sc.next();
