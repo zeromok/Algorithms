6946227eb4ae59112ed813baf08714811251f1fe
Daniel <byoung.mokk@gmail.com>
2023-08-17 20:47:12 +0900

    Refactor: 슈도 코드

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,13 +6,14 @@ public class Main {
 
 		int numCnt = Integer.parseInt(sc.next()); // 숫자의 개수 입력 받기
 		int queryCnt = Integer.parseInt(sc.next()); // 질의 개수 입력 받기
-
 		int[] sumArr = new int[ numCnt + 1 ]; // 합 배열 선언
+
 		// 합 배열 초기화
 		for ( int i = 1; i <= numCnt; i++ ) {
 			sumArr[ i ] = sumArr[ i - 1 ] + Integer.parseInt(sc.next());
 		}
 
+		// 구간 합 공식
 		for ( int q = 0; q < queryCnt; q++ ) {
 			int i = Integer.parseInt(sc.next());
 			int j = Integer.parseInt(sc.next());
