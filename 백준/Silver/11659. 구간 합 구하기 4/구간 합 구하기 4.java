4cb049b5f9b2ed2cbfb3ed4afb18a96d84cefb0b
Daniel <99172504+zeromok@users.noreply.github.com>
2023-08-17 20:43:54 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2452 ms, Memory: 136888 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main( String[] args ) {
+		Scanner sc = new Scanner( System.in );
+
+		int numCnt = Integer.parseInt(sc.next()); // 숫자의 개수 입력 받기
+		int queryCnt = Integer.parseInt(sc.next()); // 질의 개수 입력 받기
+
+		int[] sumArr = new int[ numCnt + 1 ]; // 합 배열 선언
+		// 합 배열 초기화
+		for ( int i = 1; i <= numCnt; i++ ) {
+			sumArr[ i ] = sumArr[ i - 1 ] + Integer.parseInt(sc.next());
+		}
+
+		for ( int q = 0; q < queryCnt; q++ ) {
+			int i = Integer.parseInt(sc.next());
+			int j = Integer.parseInt(sc.next());
+			System.out.println( sumArr[ j ] - sumArr[ i - 1 ] );
+		}
+	}
+
+} // end
