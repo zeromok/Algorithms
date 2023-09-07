e409d9acc389329f9de50a1fff4871ffad021cbc
Daniel <byoung.mokk@gmail.com>
2023-09-07 23:29:17 +0900

    Docs: Add comment

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -11,19 +11,22 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
 		StringTokenizer st = new StringTokenizer( br.readLine( ) );
 
+		// N 입력받기
 		long num = Integer.parseInt( st.nextToken() );
+		// 필요한 변수들 선언 및 초기화
 		long count, startIdx, endIdx, sum;
 		count = startIdx = endIdx = sum = 1;
 
+		// end_index 가 끝 점 즉, N 일때 멈추는 반복문
 		while ( endIdx != num ) {
-			if ( sum == num ) {
-				count++;
+			if ( sum == num ) { // sum == N
 				endIdx++;
 				sum += endIdx;
-			} else if ( sum > num ) {
+				count++;
+			} else if ( sum > num ) { // sum > N
 				sum -= startIdx;
 				startIdx++;
-			} else {
+			} else { // sum < N
 				endIdx++;
 				sum += endIdx;
 			}
