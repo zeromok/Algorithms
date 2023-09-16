f429a77e6b2a1c1687ba420bf145f4776e41ac9d
Daniel <byoung.mokk@gmail.com>
2023-09-17 01:51:00 +0900

    Refactor: remove

--- 백준/Bronze/1546. 평균/평균-1.java
+++ 백준/Bronze/1546. 평균/평균-1.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main( String[] args ) {
+		/*
+		 * 변수 num 에 과목수 입력받기
+		 * String strNum에 공백 기준 과목별 점수 입력받기
+		 * num의 크기를 가진 int[] scores 할당
+		 * strNum을 공백기준 배열로 만든 후 int 형변환해서 scores 에 할당
+		 * scores를 순회하며 최댓값, 모든 점수 합계 구하기
+		 * 수식을 이용해 새로운 점수의 평균 반환
+		 * */
 		Scanner sc = new Scanner( System.in );
-		// 변수 num에 과목 수 입력받기
 		int num = sc.nextInt();
-		// 길이가 num인 1차원 배열 scores 배열 선언
-		int[] scores = new int[num];
+		sc.nextLine();
+		String strNum = sc.nextLine();
 
-		for(int i=0; i<num; i++) {
-			scores[i] = sc.nextInt(); // scores[i]에 각 점수 저장 하기
+		int[] scores = new int[ num ];
+		String[] temp = strNum.split( " " );
+		for ( int i = 0; i < temp.length; i++ ) {
+			scores[ i ] = Integer.parseInt( temp[ i ] );
 		}
-
-		long m = 0;
+		long max=0;
 		long total = 0;
 		for ( int i = 0; i < scores.length; i++ ) {
-			total += scores[i]; // 총점
-			if(scores[i] > m) m = scores[i]; // 최고점
+			total += scores[ i ];
+			if(scores[i] > max)
+				max = scores[ i ];
 		}
-		System.out.println( total * 100.0 / m / num );
+		System.out.println( total * 100.0 / max / num );
 	}
-
-} // end
+}
