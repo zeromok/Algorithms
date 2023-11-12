693d4508d4b8ce25a97db0cd3f23490025b4f4c7
Daniel <99172504+zeromok@users.noreply.github.com>
2023-11-12 16:46:47 +0900

    [Silver II] Title: 스택 수열, Time: 356 ms, Memory: 26904 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main( String[] args ) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		// BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt( br.readLine() );
+
+		int[] A = new int[N];
+		for ( int i = 0; i < N; i++ ) {
+			A[ i ] = Integer.parseInt( br.readLine() );
+		}
+
+		Stack< Integer > stack = new Stack<>();
+		int num = 1;
+		boolean trigger = true;
+		for ( int i = 0; i < A.length; i++ ) {
+			if ( A[ i ] >= num ) {
+
+				while ( A[ i ] >= num ) {
+					stack.push( num++ );
+					sb.append( "+\n" );
+				}
+
+				stack.pop();
+				sb.append( "-\n" );
+			} else {
+				int top = stack.pop();
+
+				if ( top > A[ i ] ) {
+					trigger = false;
+					break;
+				} else {
+					sb.append( "-\n" );
+				}
+			}
+		}
+
+		if ( trigger ) {
+			System.out.println( sb );
+		} else {
+			System.out.println("NO");
+		}
+	}
+} // end
