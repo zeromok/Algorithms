2849275fb64d1a81f5abc78bfbe5dd04da971b9f
Daniel <99172504+zeromok@users.noreply.github.com>
2023-11-20 22:15:40 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main( String[] args ) throws IOException {
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
+
+		String N = br.readLine();
+		int[] A = new int[N.length()];
+		for ( int i = 0; i < N.length(); i++ ) {
+			A[i] = Integer.parseInt( N.substring( i, i + 1 ) );
+		}
+
+		for ( int i = 0; i < N.length(); i++ ) {
+			int maxIdx = i;
+			for ( int j = i + 1; j < N.length(); j++ ) {
+				if ( A[j] > A[maxIdx] ) {
+					maxIdx = j;
+				}
+			}
+			int temp = A[i];
+			A[i] = A[maxIdx];
+			A[maxIdx] = temp;
+		}
+
+		for(int i = 0; i < N.length(); i++) {
+            bw.write(A[i] + "");
+        }
+
+		bw.flush();
+		bw.close();
+	}
+} // end
