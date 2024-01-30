9e6fbaa80e79644c504d683cf56ab9c28f2e08a2
Daniel <byoung.mokk@gmail.com>
2024-01-30 22:42:26 +0900

    Modify: 코드서식

--- src/main/search/Node.java
+++ src/main/search/Node.java
@@ -1,4 +1,4 @@
-package src.main.search;
+package main.search;
 
 import java.util.ArrayList;
 
@@ -14,23 +14,23 @@ import java.util.ArrayList;
  * 2024-01-30           Daniel               최초 생성
  */
 class Node {
-	
+
 	// 노드의 값
 	int value;
-	
+
 	// 방문했는지?
 	boolean visited;
-	
+
 	// 노드의 인접 노드
-	ArrayList<Node> adjacentNode;
-	
-	public Node(int i, boolean b, ArrayList<Node> nodes) {
+	ArrayList< Node > adjacentNode;
+
+	public Node( int i, boolean b, ArrayList< Node > nodes ) {
 		this.value = i;
 		this.visited = b;
 		this.adjacentNode = nodes;
 	}
-	
-	public String toString() {
+
+	public String toString( ) {
 		return value + " " + visited + " " + adjacentNode;
 	}
 }
--- src/main/sort/CountingSort.java
+++ src/main/sort/CountingSort.java
@@ -11,51 +11,51 @@ public class CountingSort {
 
 		// 누적합을 이용하는 방법
 
-		long start = System.nanoTime();
+		long start = System.nanoTime( );
 		System.out.println( Arrays.toString( accSum( A ) ) );
-		long end = System.nanoTime();
+		long end = System.nanoTime( );
 		System.out.printf( "누적 합 방법 수행 후 : %f 초", ( end - start ) / 1e9 );
 
 		// 기존 방법
-		start = System.nanoTime();
+		start = System.nanoTime( );
 		System.out.println( "\n" + Arrays.toString( extracted( A ) ) );
-		end = System.nanoTime();
+		end = System.nanoTime( );
 		System.out.printf( "기존방법 수행 후 : %f 초", ( end - start ) / 1e9 );
 	}
 
 	private static int[] accSum( int[] A ) {
-		int[] B = new int[A.length + 1];
-		int[] C = new int[A.length];
+		int[] B = new int[ A.length + 1 ];
+		int[] C = new int[ A.length ];
 
 		for ( int i = 0; i < A.length; i++ ) {
-			B[A[i]]++;
+			B[ A[ i ] ]++;
 		}
 
 		for ( int i = 1; i < B.length; i++ ) {
-			B[i] = B[i] + B[i - 1];
+			B[ i ] = B[ i ] + B[ i - 1 ];
 		}
 
 		for ( int i = A.length - 1; i >= 0; i-- ) {
 			// B[A[i]]--;
-			C[-- B[A[i]]] = A[i];
+			C[ --B[ A[ i ] ] ] = A[ i ];
 		}
 		return C;
 	}
 
 	private static int[] extracted( int[] A ) {
-		int[] B = new int[A.length + 1];
-		int[] C = new int[A.length];
+		int[] B = new int[ A.length + 1 ];
+		int[] C = new int[ A.length ];
 
-		long start = System.nanoTime();
+		long start = System.nanoTime( );
 		for ( int item : A ) {
-			B[item]++;
+			B[ item ]++;
 		}
 
 		int idx = 0;
 		for ( int i = 0; i < B.length; i++ ) {
-			while ( B[i] != 0 ) {
-				B[i]--;
-				C[idx] = i;
+			while ( B[ i ] != 0 ) {
+				B[ i ]--;
+				C[ idx ] = i;
 				idx++;
 			}
 		}
