1a0b16530b3f73e80dd39f9acb445ab20d44e784
Daniel <byoung.mokk@gmail.com>
2024-04-25 17:33:29 +0900

    Modify: 서식수정

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,10 +2,13 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0)
 			return "";
+
 		Arrays.sort(strs);
+
 		String first = strs[0];
 		String last = strs[strs.length - 1];
 		int prefix = 0;
+
 		for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
 			if (first.charAt(i) == last.charAt(i)) {
 				prefix++;
@@ -13,6 +16,7 @@ class Solution {
 				break;
 			}
 		}
+
 		return prefix == 0 ? "" : first.substring(0, prefix);
     }
 }
