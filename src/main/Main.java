ff9ef47a7aae2f696c31d0d64965666581690ce0
Daniel <byoung.mokk@gmail.com>
2024-05-22 22:01:38 +0900

    Sync: Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,8 +17,25 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 
-		bw.flush();
-		bw.close();
+		int[] A = new int[N - M + 1];
+		for (int i = 0; i < A.length; i++) {
+			A[i] = M + i;
+		}
+
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			int start = Math.max(i * i, (M + i - 1) / i * i);
+			for (int k = start; k <= N; k += i) {
+				if (k >= M) {
+					A[k - M] = 0;
+				}
+			}
+		}
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] != 0 && A[i] >= 2) {
+				System.out.println(A[i]);
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
