6b0db3711476ec668a618ac6d795304205090f6f
Daniel <byoung.mokk@gmail.com>
2024-05-22 17:44:18 +0900

    Sync: Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -12,34 +13,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		int K = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 
-		long start = 1;
-		long end = (long)N * N;
-		long result = 0;
-
-		while (start <= end) {
-			long mid = (start + end) / 2;
-			long cnt = 0;
-
-			for (int i = 1; i <= N; i++) {
-				cnt += Math.min(mid / i, N);
-			}
-			if (cnt < K) {
-				start = mid + 1;
-			} else {
-				result = mid;
-				end = mid - 1;
-			}
-		}
-
-		bw.write(result + "\n");
 		bw.flush();
 		bw.close();
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		solution();
 	}
 
