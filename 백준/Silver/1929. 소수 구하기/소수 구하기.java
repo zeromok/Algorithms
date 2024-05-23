3a4399846fd6c2de0cdb6b2f3f1bcabeb0bb4682
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2024-05-23 17:39:10 +0900

    [Silver III] Title: 소수 구하기, Time: 804 ms, Memory: 31656 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -15,22 +15,21 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 
-		int[] A = new int[N - M + 1];
-		for (int i = 0; i < A.length; i++) {
-			A[i] = M + i;
+		int[] A = new int[N + 1];
+		for (int i = 2; i <= N; i++) {
+			A[i] = i;
 		}
 
 		for (int i = 2; i <= Math.sqrt(N); i++) {
-			int start = Math.max(i * i, (M + i - 1) / i * i);
-			for (int k = start; k <= N; k += i) {
-				if (k >= M) {
-					A[k - M] = 0;
-				}
+			if (A[i] == 0)
+				continue;
+			for (int k = i + i; k <= N; k = k + i) {
+				A[k] = 0;
 			}
 		}
 
-		for (int i = 0; i < A.length; i++) {
-			if (A[i] != 0 && A[i] >= 2) {
+		for (int i = M; i <= N; i++) {
+			if (A[i] != 0) {
 				System.out.println(A[i]);
 			}
 		}
