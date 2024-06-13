bc48eb287b9bd3062216666017aabdcbb2d4e962
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2024-06-13 16:45:11 +0900

    [Bronze I] Title: 최소공배수, Time: 172 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < testCase; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			int result = a * b / gcd(a, b);
+			bw.write(result + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+	}
+
+	private static int gcd(int a, int b) {
+		if (b == 0) {
+			return a;
+		}
+		return gcd(b, a % b);
+	}
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+
+} // end
