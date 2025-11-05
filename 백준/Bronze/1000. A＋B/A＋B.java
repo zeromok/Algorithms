192901b4b9cec81dc767d707c53bb45ce0fbac90
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-05 21:44:40 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Long.parseLong(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		System.out.println(A + B);
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+}
