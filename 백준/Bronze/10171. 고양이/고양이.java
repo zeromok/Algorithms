ab2150374e8aae4e424165c5c4a32a88ad681e3a
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-06 22:56:49 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("\\    /\\\n");
+		bw.write(" )  ( ')\n");
+		bw.write("(  /  )\n");
+		bw.write(" \\(__)|\n");
+
+		br.close();bw.flush();bw.close();
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+}
