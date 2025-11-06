4df2bf1b6d7b9703596b8380b4107dcb985b12d0
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-06 20:43:34 +0900

    [Bronze V] Title: N 찍기, Time: 236 ms, Memory: 21328 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			bw.write(i + "\n");
+		}
+		br.close();bw.flush(); bw.close();
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+}
