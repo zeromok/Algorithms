e32549a38a22f6234702346fe1ce5d800c6371fc
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-06 20:38:18 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13968 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,12 @@
 public class Main {
-	public static void main(String[] args) {
+
+
+	public static void solution() {
 		System.out.println("Hello World!");
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		solution();
 	}
 }
