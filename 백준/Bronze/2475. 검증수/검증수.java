9cd155129a7c7b5b6f50d69f539a4ff2923a8192
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-06 20:36:37 +0900

    [Bronze V] Title: 검증수, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		double result = 0;
+		while (st.hasMoreTokens()) {
+			result += Math.pow(Double.parseDouble(st.nextToken()), 2);
+		}
+
+		System.out.println((int)result%10);
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+}
