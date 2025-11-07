6be9100b53e019c74757b5aab07afa814bd4c2b8
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-07 12:29:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void solution() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+		int c = input.charAt(0);
+		System.out.print(c);
+
+		br.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+		solution();
+	}
+}
