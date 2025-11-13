c2e6673e6f051e5dd39dfc186a88962e2356e3c6
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-13 11:31:34 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,26 +15,25 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		int[] inputArr = new int[N];
+		int[] cards = new int[N];
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
-		for (int i = 0; i < inputArr.length; i++) {
-			inputArr[i] = Integer.parseInt(st2.nextToken());
+		for (int i = 0; i < cards.length; i++) {
+			cards[i] = Integer.parseInt(st2.nextToken());
 		}
-		int[] sortedArr = Arrays.stream(inputArr).sorted().toArray();
 
-		int resultSum = 0;
-		for (int i = 0; i < sortedArr.length - 2; i++) {
-			for (int j = i + 1; j < sortedArr.length - 1; j++) {
-				for (int k = j + 1; k < sortedArr.length; k++) {
-					int sum = sortedArr[i] + sortedArr[j] + sortedArr[k];
-					if (sum > resultSum && sum <= M) {
-						resultSum = sum;
+		int maxSum = 0;
+		for (int i = 0; i < cards.length - 2; i++) {
+			for (int j = i + 1; j < cards.length - 1; j++) {
+				for (int k = j + 1; k < cards.length; k++) {
+					int sum = cards[i] + cards[j] + cards[k];
+					if (sum > maxSum && sum <= M) {
+						maxSum = sum;
 					}
 				}
 			}
 		}
 
-		bw.write(resultSum + "");
+		bw.write(maxSum + "");
 
 		br.close();
 		bw.flush();
