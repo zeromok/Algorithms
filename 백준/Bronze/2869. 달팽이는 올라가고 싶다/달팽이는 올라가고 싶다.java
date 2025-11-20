3501fa6ffc6dbd288cf01b16d81e8199c7e3759b
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-11-20 13:25:59 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+
+		bw.write((V - B - 1) / (A - B) + 1 + "");
+
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
