19ef99daf04a46c526f7f6e17d3e5eba94a8bed7
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-12-01 17:42:29 +0900

    [Bronze III] Title: 웰컴 키트, Time: 128 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void solution() throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] sizes = new int[6];
+		StringTokenizer st1 = new StringTokenizer(br.readLine());
+		for (int i = 0; i < sizes.length; i++) {
+			sizes[i] = Integer.parseInt(st1.nextToken());
+		}
+
 		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st2.nextToken());
 		int P = Integer.parseInt(st2.nextToken());
 
 		int tShirtBundles = 0;
-		for (int i = 0; i < 6; i++) {
-			int orderNum = Integer.parseInt(st.nextToken());
-			if (orderNum > 0) {
-				tShirtBundles += (orderNum + T - 1) / T;
-			}
+		for (int size : sizes) {
+			tShirtBundles += (size + T - 1) / T;
 		}
-
 		int penBundles = N / P;
 		int penPieces = N % P;
 
@@ -34,9 +35,5 @@ public class Main {
 		br.close();
 		bw.flush();
 		bw.close();
-	}
-
-	public static void main(String[] args) throws Exception {
-		solution();
 	}
 }
