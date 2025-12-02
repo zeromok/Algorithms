25a7f012672f6601032b81ce11c4165969867715
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-12-02 09:08:58 +0900

    [Bronze III] Title: 웰컴 키트, Time: 128 ms, Memory: 16092 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -11,7 +11,7 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		
+
 		int[] sizes = new int[6];
 		StringTokenizer st1 = new StringTokenizer(br.readLine());
 		for (int i = 0; i < sizes.length; i++) {
@@ -22,18 +22,23 @@ public class Main {
 		int T = Integer.parseInt(st2.nextToken());
 		int P = Integer.parseInt(st2.nextToken());
 
-		int tShirtBundles = 0;
-		for (int size : sizes) {
-			tShirtBundles += (size + T - 1) / T;
-		}
-		int penBundles = N / P;
-		int penPieces = N % P;
+		int totalTShirtBundles = calTShirtBundles(sizes, T);
+		int penBundleCount = N / P;
+		int penIndividualCount = N % P;
 
-		bw.write(tShirtBundles + "\n");
-		bw.write(penBundles + " " + penPieces);
+		bw.write(totalTShirtBundles + "\n");
+		bw.write(penBundleCount + " " + penIndividualCount);
 
 		br.close();
 		bw.flush();
 		bw.close();
+	}
+
+	private static int calTShirtBundles(int[] sizes, int bundleSize) {
+		int total = 0;
+		for (int size : sizes) {
+			total += (size + bundleSize - 1) / bundleSize;
+		}
+		return total;
 	}
 }
