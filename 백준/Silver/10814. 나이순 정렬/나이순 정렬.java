a1ced17dfcfed23e3d3695f1124a6fe46cd6d7cc
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-12-10 22:47:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 596 ms, Memory: 46736 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -2,87 +2,67 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
+	static class Member {
+		int age;
+		String  name;
 
+		public Member(int age, String name) {
+			this.age = age;
+			this.name = name;
+		}
+	}
+	static Member[] temp;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-		int N = Integer.parseInt(br.readLine());
+			int N = Integer.parseInt(br.readLine());
+			Member[] members = new Member[N];
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				members[i] = new Member(Integer.parseInt(st.nextToken()), st.nextToken());
+			}
 
-		String[] members = new String[N];
-		for (int i = 0; i < N; i++) {
-			members[i] = br.readLine();
+			temp = new Member[N];
+			recursive(members, 0, N - 1);
+
+			for (Member member : members) {
+				bw.write(member.age + " " + member.name + "\n");
+			}
+
+			bw.flush();
 		}
-
-		mergeSort(members, 0, members.length - 1);
-
-		for (String member : members) {
-			bw.write(member+ "\n");
-		}
-
-		br.close();
-		bw.flush();
-		bw.close();
 	}
 
-	private static void mergeSort(String[] members, int left, int right) {
-		// 종료 조건
+	private static void recursive(Member[] arr, int left, int right) {
 		if (left < right) {
-			// 분할
 			int mid = (left + right) / 2;
 
-			// 재귀 호출
-			mergeSort(members, left, mid);
-			mergeSort(members, mid + 1, right);
+			recursive(arr, left, mid);
+			recursive(arr, mid + 1, right);
 
-			// 병합
-			merge(members, left, mid, right);
+			merge(arr, left, mid, right);
 		}
 	}
 
-	private static void merge(String[] members, int left, int mid, int right) {
-		// 임시 배열 생성
-		int n1 = mid - left + 1;
-		int n2 = right - mid;
-		String[] L = new String[n1];
-		String[] R = new String[n2];
+	private static void merge(Member[] arr, int left, int mid, int right) {
+		System.arraycopy(arr, left, temp, left, (right - left) + 1);
 
-		// 데이터 복사
-		for (int i = 0; i < n1; i++) {
-			L[i] = members[left + i];
-		}
-		for (int i = 0; i < n2; i++) {
-			R[i] = members[mid + 1 + i];
-		}
+		int i = left, j = mid + 1, k = left;
 
-		// 병합
-		int i = 0, j = 0;
-		int k = left;
-		while (i < n1 && j < n2) {
-			int l1 = Integer.parseInt(L[i].split(" ")[0]);
-			int r1 = Integer.parseInt(R[j].split(" ")[0]);
-			if (l1 <= r1) {
-				members[k] = L[i];
-				i++;
+		while (i <= mid && j <= right) {
+			if (temp[i].age <= temp[j].age) {
+				arr[k++] = temp[i++];
 			} else {
-				members[k] = R[j];
-				j++;
+				arr[k++] = temp[j++];
 			}
-			k++;
 		}
 
-		// 남은 원소 복사
-		while (i < n1) {
-			members[k] = L[i];
-			i++;
-			k++;
-		}
-		while (j < n2) {
-			members[k] = R[j];
-			j++;
-			k++;
+		while (i <= mid) {
+			arr[k++] = temp[i++];
 		}
 	}
 }
