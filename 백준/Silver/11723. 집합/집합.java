76f5058a20e7df55abe68adbccbea547a8aa79ef
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-12-11 16:24:01 +0900

    [Silver V] Title: 집합, Time: 1248 ms, Memory: 300668 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,63 +2,48 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
+	static int[] data;
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-		Set<Integer> set = new HashSet<>();
-		int T = Integer.parseInt(br.readLine());
-		for (int i = 0; i < T; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int output = fulfillment(st, set);
-			if (output != -1) {
-				bw.write(output + "\n");
-			}
-		}
+			int S = 0;
+			int M = Integer.parseInt(br.readLine());
 
-		br.close();
-		bw.flush();
-		bw.close();
-	}
+			for (int i = 0; i < M; i++) {
+				String[] input = br.readLine().split(" ");
+				String cmd = input[0];
 
-	private static int fulfillment(StringTokenizer st, Set<Integer> set) {
-		String command = st.nextToken();
-		int result = -1;
-		switch (command) {
-			case "add":
-				set.add(Integer.parseInt(st.nextToken()));
-				break;
-			case "remove":
-				set.remove(Integer.parseInt(st.nextToken()));
-				break;
-			case "check":
-				result = set.contains(Integer.parseInt(st.nextToken())) ? 1 : 0;
-				break;
-			case "toggle": {
-				int e = Integer.parseInt(st.nextToken());
-				if (set.contains(e)) {
-					set.remove(e);
-				} else {
-					set.add(e);
+				switch (cmd) {
+					case "add":
+						int add = Integer.parseInt(input[1]) - 1;
+						S |= (1 << add);
+						break;
+					case "remove":
+						int remove = Integer.parseInt(input[1]) - 1;
+						S &= ~(1 << remove);
+						break;
+					case "check":
+						int check = Integer.parseInt(input[1]) - 1;
+						bw.append(String.valueOf((S & (1 << check)) != 0 ? 1 : 0)).append("\n");
+						break;
+					case "toggle":
+						int toggle = Integer.parseInt(input[1]) - 1;
+						S ^= (1 << toggle);
+						break;
+					case "all":
+						S = (1 << 20) - 1;
+						break;
+					case "empty":
+						S = 0;
+						break;
 				}
 			}
-			break;
-			case "all": {
-				set.clear();
-				for (int i = 1; i < 21; i++) {
-					set.add(i);
-				}
-			}
-			break;
-			case "empty":
-				set.clear();
-				break;
+
+			bw.flush();
 		}
-		return result;
 	}
 }
