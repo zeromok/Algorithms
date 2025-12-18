548f31ad79b52028f203439da505d7089c4b3dd7
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2025-12-18 21:44:34 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -4,25 +4,32 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+	static int[] memo;
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
+			memo = new int[1_000 + 1];
+			memo[1] = 1;
+			memo[2] = 2;
 			int N = Integer.parseInt(br.readLine());
-			long[] dp = new long[1_000 + 1];
-			bottomUp(dp);
 
-			bw.write(dp[N] + "\n");
+			bw.write(topDown(N) + "");
 
 			bw.flush();
 		}
 	}
 
-	private static void bottomUp(long[] dp) {
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= 1000; i++) {
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10_007;
+	private static int topDown(int n) {
+		if (n == 1) {
+			return 1;
 		}
+
+		if (memo[n] != 0) {
+			return memo[n];
+		}
+
+		memo[n] = (topDown(n - 1) + topDown(n - 2)) % 10_007;
+		return memo[n];
 	}
 }
