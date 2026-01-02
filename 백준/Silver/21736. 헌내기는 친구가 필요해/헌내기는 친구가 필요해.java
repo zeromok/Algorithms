d51b85329511c2287d5bff4853b818c6ea3610ae
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-01-02 14:24:36 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 336 ms, Memory: 37172 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -9,9 +9,12 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N;
 	static int M;
-	static String[][] CAMPUS;
-	static boolean[][] VISITED;
 	static int COUNT;
+	static char[][] CAMPUS;
+	static boolean[][] VISITED;
+	static final int[] DX = {-1, 1, 0, 0};
+	static final int[] DY = {0, 0, -1, 1};
+
 	static class Point {
 		int x;
 		int y;
@@ -21,6 +24,7 @@ public class Main {
 			this.y = y;
 		}
 	}
+
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
@@ -28,32 +32,28 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			CAMPUS = new String[N][M];
+			CAMPUS = new char[N][M];
 			VISITED = new boolean[N][M];
 
+			int startX = 0, startY = 0;
 			for (int i = 0; i < N; i++) {
-				char[] charArray = br.readLine().toCharArray();
+				char[] oneLine = br.readLine().toCharArray();
 				for (int j = 0; j < M; j++) {
-					CAMPUS[i][j] = String.valueOf(charArray[j]);
-				}
-			}
-
-			int x = 0, y = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (CAMPUS[i][j].equals("I")) {
-						x = i;
-						y = j;
+					if (oneLine[j] == 'I') {
+						startX = i;
+						startY = j;
 					}
+					CAMPUS[i][j] = oneLine[j];
 				}
 			}
 
-			bfs(x, y);
+			bfs(startX, startY);
 
-			if (COUNT == 0)
+			if (COUNT == 0) {
 				bw.write("TT");
-			else
+			} else {
 				bw.write(COUNT + "");
+			}
 			bw.flush();
 		}
 	}
@@ -64,19 +64,16 @@ public class Main {
 		VISITED[x][y] = true;
 
 		while (!queue.isEmpty()) {
-			Point currPoint = queue.poll();
-
-			int[] DX = {-1, 1, 0, 0};
-			int[] DY = {0, 0, -1, 1};
+			Point curr = queue.poll();
 
 			for (int i = 0; i < 4; i++) {
-				int nx = currPoint.x + DX[i];
-				int ny = currPoint.y + DY[i];
+				int nx = curr.x + DX[i];
+				int ny = curr.y + DY[i];
 
-				if (nx >= 0 && ny >= 0 && nx < N && ny < M && !CAMPUS[nx][ny].equals("X") && !VISITED[nx][ny]) {
-					VISITED[nx][ny] = true;
+				if (nx >= 0 && ny >= 0 && nx < N && ny < M && !VISITED[nx][ny] && CAMPUS[nx][ny] != 'X') {
 					queue.offer(new Point(nx, ny));
-					if (CAMPUS[nx][ny].equals("P")) {
+					VISITED[nx][ny] = true;
+					if (CAMPUS[nx][ny] == 'P') {
 						COUNT++;
 					}
 				}
