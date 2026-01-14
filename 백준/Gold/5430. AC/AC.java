e2ce69e4cca79d6bae4ef431e7af62117a1ce071
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-01-14 17:01:37 +0900

    [Gold V] Title: AC, Time: 736 ms, Memory: 83704 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,66 +1,66 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static void main(String[] args) throws Exception {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-	public static void solution() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+			int T = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt(br.readLine());
+			while (T-- > 0) {
+				String p = br.readLine();
+				int n = Integer.parseInt(br.readLine());
+				Deque<Integer> deque = parseArrToDeque(n, br.readLine());
 
-		while (T-- > 0) {
-			String result = processTestCase(br);
-			sb.append(result).append('\n');
+				bw.write(solve(p, deque));
+			}
+			bw.flush();
 		}
-
-		System.out.print(sb);
-		br.close();
 	}
 
-	private static String processTestCase(BufferedReader br) throws IOException {
-		String p = br.readLine();
-		int n = Integer.parseInt(br.readLine());
-		String arrStr = br.readLine();
-
-		Deque<Integer> deque = parseArrayToDeque(n, arrStr);
-
+	private static String solve(String p, Deque<Integer> deque) {
 		boolean isReversed = false;
-		boolean isError = false;
 
 		for (char cmd : p.toCharArray()) {
 			if (cmd == 'R') {
 				isReversed = !isReversed;
-			} else if (cmd == 'D') {
+			} else {
 				if (deque.isEmpty()) {
-					isError = true;
-					break;
+					return "error\n";
 				}
-				executeDiscard(deque, isReversed);
+				if (isReversed) {
+					deque.pollLast();
+				} else {
+					deque.pollFirst();
+				}
 			}
 		}
 
-		if (isError) {
-			return "error";
-		} else {
-			return formatResult(deque, isReversed);
-		}
+		return makeOutputString(deque, isReversed);
 	}
 
-	private static void executeDiscard(Deque<Integer> deque, boolean isReversed) {
-		if (isReversed) {
-			deque.pollLast();
-		} else {
-			deque.pollFirst();
+	private static String makeOutputString(Deque<Integer> deque, boolean isReversed) {
+
+		StringBuilder sb = new StringBuilder("[");
+		while (!deque.isEmpty()) {
+			// 뒤집힌 상태면 뒤에서부터, 아니면 앞에서부터 하나씩 꺼내어 붙임
+			sb.append(isReversed ? deque.pollLast() : deque.pollFirst());
+			if (!deque.isEmpty()) {
+				sb.append(",");
+			}
 		}
+		sb.append("]\n");
+
+		return sb.toString();
 	}
 
-	private static Deque<Integer> parseArrayToDeque(int n, String arrStr) {
+	private static Deque<Integer> parseArrToDeque(int n, String arrStr) {
 		Deque<Integer> deque = new ArrayDeque<>();
 		if (n > 0) {
 			String numbers = arrStr.substring(1, arrStr.length() - 1);
@@ -70,33 +70,5 @@ public class Main {
 			}
 		}
 		return deque;
-	}
-
-	private static String formatResult(Deque<Integer> deque, boolean isReversed) {
-		StringBuilder result = new StringBuilder("[");
-
-		if (!deque.isEmpty()) {
-			Iterator<Integer> it;
-			// Reverse 상태에 따라 반복자 선택
-			if (isReversed) {
-				it = deque.descendingIterator();
-			} else {
-				it = deque.iterator();
-			}
-
-			while (it.hasNext()) {
-				result.append(it.next());
-				if (it.hasNext()) {
-					result.append(',');
-				}
-			}
-		}
-
-		result.append(']');
-		return result.toString();
-	}
-
-	public static void main(String[] args) throws Exception {
-		solution();
 	}
 }
